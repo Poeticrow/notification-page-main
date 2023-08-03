c5b8441c1e18732185d3ceef0648751df02f3161
Poeticrow <ebubenwanze@gmail.com>
2023-08-03 08:20:30 +0100

    formatted a little bit

--- script.js
+++ script.js
@@ -1,66 +1,55 @@
-"use strict"
+"use strict";
 
-
-const notifications = document.querySelectorAll('.notif-section');
-const read = document.querySelector('.mark');
-const unreadMessages = document.querySelector('.unread-tag');
-const message = document.querySelector('.message')
-
+const notifications = document.querySelectorAll(".notif-section");
+const read = document.querySelector(".mark");
+const unreadMessages = document.querySelector(".unread-tag");
+const message = document.querySelector(".message");
 
 const readM = (not, i) => {
-    not.classList.remove('unread')
-    if(i = 3) {
-        message.classList.remove('hidden')
-    } else (
-        message.classList.add('hidden')
-        )
-}
-window.addEventListener('DOMContentLoaded', function() {
-    
-    // const newNotifs = notifs.map(notif => notif.classList.add('unread'))
-    
-    notifications.forEach((notification,i , arr) => {
-        // notification.className += 'unread'
-        // notification.classList.add('unread')
-        // if(i = 3) {
-        //     message.classList.add('hidden')
-        // }
-        if(!notification.classList.contains('unread')) {
-        notification.className += ' unread'
+  not.classList.remove("unread");
+  if ((i = 3)) {
+    message.classList.remove("hidden");
+  } else message.classList.add("hidden");
+};
+window.addEventListener("DOMContentLoaded", function () {
+  // const newNotifs = notifs.map(notif => notif.classList.add('unread'))
+
+  notifications.forEach((notification, i, arr) => {
+    // notification.className += 'unread'
+    // notification.classList.add('unread')
+    // if(i = 3) {
+    //     message.classList.add('hidden')
+    // }
+    if (!notification.classList.contains("unread")) {
+      notification.className += " unread";
+      if ((i = 3)) {
+        message.classList.add("hidden");
+      }
     }
-})
-})
+  });
+});
 
-const notifs = Array.from(notifications)
+const notifs = Array.from(notifications);
 
+read.addEventListener("click", function () {
+  // console.log('clicked')
+  notifications.forEach((notification, i, arr) => {
+    readM(notification, i);
+  });
+});
 
-read.addEventListener('click', function() {
-    // console.log('clicked')
-    notifications.forEach((notification, i, arr) => {
-       readM(notification, i)
-    })
-})
-
-notifications.forEach((notification, i, arr) => notification.addEventListener('click', function() {
-    readM(notification, i)
-    }))
-
-
+notifications.forEach((notification, i, arr) =>
+  notification.addEventListener("click", function () {
+    readM(notification, i);
+  })
+);
 
 // const unreadMess = newNotifs.filter((notif)=> {
 //     notif.classList.contains('unread')
 // })
- console.log(notifs);
+console.log(notifs);
 //  console.log(unreadMess);
- 
 
-const take = notifs[6].classList.contains('unread');
-
+const take = notifs[6].classList.contains("unread");
 
 console.log(take);
-
-
-
-
-
-
