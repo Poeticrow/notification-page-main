422966b69868cdab2e112bbf50814de4c0537152
Poeticrow <ebubenwanze@gmail.com>
2023-08-03 22:59:41 +0100

    working script but faulty css and html(no red dot)

--- script.js
+++ script.js
@@ -4,64 +4,44 @@ const notifications = document.querySelectorAll(".notif-section");
 const read = document.querySelector(".mark");
 const unreadMessages = document.querySelector(".unread-tag");
 const message = document.querySelector(".message");
-const noti1 = document.querySelector(".noti1");
-const noti2 = document.querySelector(".noti2");
-const noti3 = document.querySelector(".noti3");
+// const noti1 = document.querySelector(".noti1");
+// const noti2 = document.querySelector(".noti2");
+// const noti3 = document.querySelector(".noti3");
+let unread = 0;
 
-read.addEventListener("click", function(){
-  read.style.color = "hsl(219, 9%, 63%);"
+notifications.forEach((notification, i) => {
+  if (!notification.classList.contains("unread")) {
+    notification.classList.add("unread");
+  }
+  message.classList.add("hidden");
+  unread++;
+  unreadMessages.textContent = unread;
+});
 
-  noti1.classList.remove("unread");
-  noti2.classList.remove("unread");
-  noti3.classList.remove("unread");
+
+read.addEventListener("click", function () {
+  read.style.color = "hsl(219, 9%, 63%);";
+  notifications.forEach((notification) => {
+    notification.classList.remove("unread");
+    message.classList.remove("hidden");
+  });
+  unread = 0;
   unreadMessages.textContent = "0";
-})
+});
 
-// const readM = (not, i) => {
-//   not.classList.remove("unread");
-//   if ((i = 3)) {
-//     message.classList.remove("hidden");
-//   } else message.classList.add("hidden");
-// };
-// window.addEventListener("DOMContentLoaded", function () {
-//   // const newNotifs = notifs.map(notif => notif.classList.add('unread'))
+notifications.forEach((notification, i) => {
+  notification.addEventListener("click", function () {
+    if (notification.classList.contains("unread")) {
+      unread--;
+    }
+    unreadMessages.textContent = unread;
+    notification.classList.remove("unread");
+    if (
+      /*
+      notification.querySelector(".message").classList.contains("message") &&*/
+      i === 3
+    )
+      message.classList.remove("hidden");
+  });
+});
 
-//   notifications.forEach((notification, i, arr) => {
-//     // notification.className += 'unread'
-//     // notification.classList.add('unread')
-//     // if(i = 3) {
-//     //     message.classList.add('hidden')
-//     // }
-//     if (!notification.classList.contains("unread")) {
-//       notification.className += " unread";
-//       if ((i = 3)) {
-//         message.classList.add("hidden");
-//       }
-//     }
-//   });
-// });
-
-// const notifs = Array.from(notifications);
-
-// read.addEventListener("click", function () {
-//   // console.log('clicked')
-//   notifications.forEach((notification, i, arr) => {
-//     readM(notification, i);
-//   });
-// });
-
-// notifications.forEach((notification, i, arr) =>
-//   notification.addEventListener("click", function () {
-//     readM(notification, i);
-//   })
-// );
-
-// // const unreadMess = newNotifs.filter((notif)=> {
-// //     notif.classList.contains('unread')
-// // })
-// console.log(notifs);
-// //  console.log(unreadMess);
-
-// const take = notifs[6].classList.contains("unread");
-
-// console.log(take);
